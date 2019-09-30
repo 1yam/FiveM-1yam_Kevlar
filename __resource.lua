3762b3b3ab7e8586fd212594e9c6eb6102579501
1yam <40899431+1yam@users.noreply.github.com>
2019-09-30 17:53:55 +0200

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,19 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+description 'Script dévelloper par Px1101 => KevLar '
+
+version '0.0.1'
+
+client_scripts {
+    '@es_extended/locale.lua',
+    'client/cl_kevlar.lua'
+}
+
+server_scripts {
+	'@mysql-async/lib/MySQL.lua',
+    '@es_extended/locale.lua',
+    'server/sv_kevlar.lua'
+}
+
+
+dependencies 'es_extended'
