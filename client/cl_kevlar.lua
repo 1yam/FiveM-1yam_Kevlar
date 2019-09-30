032eb598e1241aa876ac19f25c260eba5213945e
1yam <40899431+1yam@users.noreply.github.com>
2019-09-30 22:22:53 +0200

    Update cl_kevlar.lua

--- client/cl_kevlar.lua
+++ client/cl_kevlar.lua
@@ -113,29 +113,6 @@ function Notify(text)
     AddTextComponentString(text)
     DrawNotification(false, false)
 end
---Sound By Plunkettscote
-
-RegisterNetEvent('InteractSound_CL:PlayWithinDistance')
-AddEventHandler('InteractSound_CL:PlayWithinDistance', function(playerNetId, maxDistance, soundFile, soundVolume)
-    local lCoords = GetEntityCoords(GetPlayerPed(-1))
-    local eCoords = GetEntityCoords(GetPlayerPed(GetPlayerFromServerId(playerNetId)))
-    local distIs  = Vdist(lCoords.x, lCoords.y, lCoords.z, eCoords.x, eCoords.y, eCoords.z)
-    if(distIs <= maxDistance) then
-        SendNUIMessage({
-            transactionType     = 'playSound',
-            transactionFile     = soundFile,
-            transactionVolume   = soundVolume
-        })
-    end
-end)
-RegisterNetEvent('InteractSound_CL:PlayOnAll')
-AddEventHandler('InteractSound_CL:PlayOnAll', function(soundFile, soundVolume)
-    SendNUIMessage({
-        transactionType     = 'playSound',
-        transactionFile     = soundFile,
-        transactionVolume   = soundVolume
-    })
-end)
 
 print("           Script By Px1101")
 print("_______$$$$$$$$______$$$$$$$$$___ ")
